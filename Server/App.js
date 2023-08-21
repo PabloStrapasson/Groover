a51b00db8d1443e4b8830ad7b142c2dd85e589fc
Pablo Strapasson <pablostrapasson98@gmail.com>
2023-08-21 18:00:17 -0300

    server node

---
+++ Server/App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+router.get('/search', (req, res) => {
+    res.send('search page');
+})
+
+app.listen(port, () => {console.log(`Rodando servidor na porta ${port}`)});
+
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+app.use(express.static(path.join(__dirname, '/public')));
